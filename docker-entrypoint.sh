2cec2b661f1897aa3f4e2fd595b788a8bb22f16e
Jordan Lancheros <jordan1703@hotmail.es>
2020-09-06 21:15:47 -0500

    change ip sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /usr/src/app/web/vue/dist/UIconfig.js
-sed -i 's/localhost/'${HOST}'/g' /usr/src/app/web/vue/dist/UIconfig.js
+sed -i 's/68.183.115.60/0.0.0.0/g' /usr/src/app/web/vue/dist/UIconfig.js
+sed -i 's/68.183.115.60/'${HOST}'/g' /usr/src/app/web/vue/dist/UIconfig.js
 sed -i 's/3000/'${PORT}'/g' /usr/src/app/web/vue/dist/UIconfig.js
 if [[ "${USE_SSL:-0}" == "1" ]] ; then
     sed -i 's/ssl: false/ssl: true/g' /usr/src/app/web/vue/dist/UIconfig.js
